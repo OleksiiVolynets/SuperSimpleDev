e5de5ef8f72e94baa659b0995c01e94d1e032526
OleksiiVolynets <pereguznya@gmail.com>
2026-08-23 15:30:59 +0200

    create a test for deleteFromCart function

--- javascript-amazon-project-main/tests-jasmine/data/cartTest.js
+++ javascript-amazon-project-main/tests-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
+import { addToCart, cart, loadFromStorage, deleteFromCart } from '../../data/cart.js';
 
 describe('AddToCart function', ()=>{
  beforeEach(() => {
@@ -39,5 +39,39 @@ describe('AddToCart function', ()=>{
     let stringCart = JSON.stringify(cart)
     expect(localStorage.setItem).toHaveBeenCalledWith('cart6',stringCart)
 
+  })
+})
+
+describe('deleteFromCart function',() => {
+  beforeEach(() => {
+    spyOn(localStorage,'setItem');
+    spyOn(localStorage,'getItem').and.callFake(() =>   JSON.stringify([{
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 4,
+        deliveryOptionId:'1'
+      },{
+        productId: '54e0eccd-8f36-462b-b68a-8182611d9add',
+        quantity: 6,
+        deliveryOptionId:'3'
+      }])
+    )
+  })
+
+  it('delete an existing product in the cart', () => {
+
+
+    loadFromStorage();
+    deleteFromCart('15b6fc6f-327a-4ec4-896f-486349e85a3d');
+    expect(cart.length).toEqual(1);
+
+  })
+
+  it('delete a not existing product in the cart', () => {
+
+
+    loadFromStorage();
+    deleteFromCart('dd82ca78-a18b-4e2a-9250-31e67412f98d');
+    expect(cart.length).toEqual(2);
+    
   })
 })
