d3ccd583b7689c95bac828b535f88b39f32db5c6
OleksiiVolynets <pereguznya@gmail.com>
2026-08-27 22:37:59 +0200

    add parametrs to constructor

--- javascript-amazon-project-main/data/cart-class.js
+++ javascript-amazon-project-main/data/cart-class.js
@@ -65,11 +65,9 @@ class Cart {
 
 
 
-const cart = new Cart();
+const cart = new Cart('cart-oop');
 
-const businessCart = new Cart();
-
-
+const businessCart = new Cart('cart-business');
 
 
 console.log(cart);
