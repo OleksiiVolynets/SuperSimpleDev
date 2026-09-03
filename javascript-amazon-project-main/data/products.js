a727fff2d3bca87af68ba6655454b3c540430c41
OleksiiVolynets <pereguznya@gmail.com>
2026-09-03 14:21:54 +0200

    create some practise code to learn more details about classes and this

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -45,6 +45,39 @@ class Clothing extends Product {
   }
 }
 
+// const date=new Date();
+// console.log(date);
+// console.log(date.toLocaleTimeString())
+
+// console.log(this);
+
+// const object2= {
+//   a:2,
+//   b: this.a
+// }
+
+
+// function logThis() {
+//   console.log(this)
+// }
+// logThis() ;
+// logThis.call('hello');
+
+
+// const object3 ={
+
+//   func() {
+//     console.log(this)
+//   },
+//   method: () => {
+//     console.log(this)
+//   }
+// }
+// object3.func()
+// object3.method();
+
+
+
 
 export const products = [
   {
