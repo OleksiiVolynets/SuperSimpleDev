514df5b9b42b36bec54dc2dfb52ba4b8ee6cdced
OleksiiVolynets <pereguznya@gmail.com>
2026-09-19 16:19:26 +0200

    add a new property to class

--- javascript-amazon-project-main/data/cart-car.js
+++ javascript-amazon-project-main/data/cart-car.js
@@ -4,20 +4,46 @@
 class Car{
   brand;
   model;
+  speed;
 
-  constructor(brand, model) {
-    this.brand=brand;
-    this.model=model;
+  constructor(brand, model, speed) {
+    this.brand = brand;
+    this.model = model;
+    if(speed<200 && speed>0) {
+      this.speed = speed;
+    }
+    else{
+      console.log("Speed should be lower than 200 and higher than 0 km/h ")
+    }
   }
 
   displayMethod() {
-    console.log(`Brand: ${this.brand}, Model: ${this.model}`)
+    console.log(`Brand: ${this.brand}, Model: ${this.model}, Speed: ${this.speed} km/h`)
+  }
+
+  go() {
+    let newSpeed = this.speed+5;
+    if(newSpeed<200){
+      this.speed+=5;
+    }
+    else{
+      console.log("Speed should be lower than 200 and higher than 0 km/h ")
+    }
+  }
+  brake() {
+    let newSpeed = this.speed-5;
+    if(newSpeed>0){
+      this.speed-=5;
+    }
+    else{
+      console.log("Speed should be lower than 200 and higher than 0 km/h ")
+    }
   }
 }
 
-const toyota = new Car('Toyota', 'Corolla');
-const tesla = new Car('Tesla', 'Model 3');
-
+const toyota = new Car('Toyota', 'Corolla', 140);
+const tesla = new Car('Tesla', 'Model 3', 160);
+toyota.go();
 
 toyota.displayMethod();
 tesla.displayMethod();
